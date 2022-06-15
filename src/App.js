778a26af4f682cce3bbf3d026a09e1706cf61166
Ben G Vuong <78512101+BenVuong@users.noreply.github.com>
2022-06-15 08:48:17 -0500

    Added button to add new expense

--- src/App.js
+++ src/App.js
@@ -1,20 +1,59 @@
-import React from 'react';
-import ExpenseItem from './components/ExpenseItem';
+import React from "react";
+import ExpenseItem from "./components/ExpenseItem";
+import NewExpense from "./components/NewExpense";
 //import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Computer",
+      amount: 1000,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e2",
+      title: "New Monitor",
+      amount: 250,
+      date: new Date(2021, 2, 12),
+    },
+    {
+      id: "e3",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
+  let total = expenses[0].amount + expenses[1].amount + expenses[2].amount;
   return (
     <div className="App">
       <header className="App-header">
-        
-        <h2>
-        Hello world
-        </h2>
-        <ExpenseItem>
-          
+        <h2>An Expense Tracking Application</h2>
+        <NewExpense/>
+        <ExpenseItem
+          title={expenses[0].title}
+          amount={expenses[0].amount}
+          date={expenses[0].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={expenses[1].title}
+          amount={expenses[1].amount}
+          date={expenses[1].date}
+        ></ExpenseItem>
+        <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+        >
+
         </ExpenseItem>
-        
+        <ExpenseItem
+          title={"Total"}
+          amount={total}
+          date={expenses[2].date}
+        ></ExpenseItem>
       </header>
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,32 @@
-import React from 'react';
-import './ExpenseItem.css';
-function ExpenseItem() {
-    return (
-    <div className="expense-item">
-        <div className="expense-item h2 ">
-            <h2> Item Title: </h2>
-        </div>
-        <div className="expense-item__description">
-            TV
-        </div>
-    
-    </div>
-    );
+import React, { useState } from "react";
+import ExpensenDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+import Card from './Card';
+const ExpenseItem = (props) => {
 
+  const [title, setTitle] = useState(props.title);
+
+  
+  const clickHandler = () => {
+    setTitle("Title is updated");
+    console.log(title);
+  };
+
+
+  return (
+    <Card className="expense-item">
+      <div>
+        {" "}
+        <ExpensenDate date={props.date} />{" "}
+      </div>
+
+      <div className="expense-item__description">
+        <h2> {title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
 }
 
 export default ExpenseItem;
